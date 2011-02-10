50cee7ac1285e7eb8c3b5cd32db817cc0dc49d67
segfault <razor@localhost.(none)>
2011-02-11 00:37:08 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -3,26 +3,33 @@
 
 from xml.dom.minidom import parse
 import gtk
+import sqlite3
 
 class main_ui():
     def __init__(self):
         a = gtk.Builder()
         a.add_from_file("main_ui.glade")
         self.window = a.get_object("main_window")
-        self.axce1 = a.get_object("get_axcel")
-        self.segfault = a.get_object("get_seg")
+        self.axce1 = a.get_object("gen_axcel")
+        self.segfault = a.get_object("gen_seg")
         self.choose_file = a.get_object("choose_file")
         self.buffer = a.get_object("buffer")
         self.window.connect("destroy", gtk.main_quit)
         self.choose_file.connect("file-set", self.file_set)
-        self.segfault.connect("clicked", self.segfault_clicked)
-        self.axce1.connect("clicked", self.axce1_clicked)
+        self.segfault.connect("clicked", self.clicked, self._gen_seg)
+        self.axce1.connect("clicked", self.clicked, self._gen_axcel)
 
-    def segfault_clicked(self, button):
-        self.buffer.insert(self.buffer.get_end_iter(), "{0}\n".format("segfault"))
+    def _gen_seg(self):
+        return "segfault\tseg\tsegfaulting in progress"
 
-    def axce1_clicked(self, button):
-        pass
+    def _gen_axcel(self):
+        return "axcel"
+
+    def clicked(self, button, call_me):
+        if hasattr(self, "coats") and self.coats.checked:
+            self.buffer.set_text(call_me())
+        else:
+            self.show_error(u'Сначала надо указать валидный файл')
 
     def show_error(self, text):
         dial = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons = gtk.BUTTONS_OK, flags=gtk.DIALOG_MODAL, parent = self.window)
@@ -50,6 +57,7 @@ class main_ui():
 class xml_parser():
     def __init__(self, filename):
         self.xml = parse(filename)
+        self.checked = False
 
     def check_file(self):
         if not (self.xml.childNodes.length == 1 and self.xml.childNodes[0].nodeName == "report"):
@@ -58,10 +66,12 @@ class xml_parser():
         for name in ["common_deal", "briefcase_position", "account_totally_line"]:
             if self.report.getElementsByTagName(name).length != 1:
                 raise Exception("there is no {0} in report or more that one found".format(name))
-        self.common_deal = self.report.getElementsByTagName("common_deal")[0]
-        self.account_totally = self.report.getElementsByTagName("account_totally_line")[0]
-        self.briefcase = self.report.getElementsByTagName("briefcase_position")[0]
-        
+        self.common_deal = self.report.getElementsByTagName("common_deal")[0].getElementsByTagName("item")
+        self.account_totally = self.report.getElementsByTagName("account_totally_line")[0].getElementsByTagName("item")
+        self.briefcase = self.report.getElementsByTagName("briefcase_position")[0].getElementsByTagName("item")
+        if not (self.common_deal.length > 0 and self.account_totally.length > 1 and self.briefcase.length > 1):
+            raise Exception(u'Странное количество тегов item в отчете, либо отчет битый, либо это вобще не отчет')
+        self.checked=True
         
 
 
