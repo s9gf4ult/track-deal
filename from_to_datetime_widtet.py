866486bead3abb0e7a51e950259b44055b9e3529
segfault <s9gf4ult@gmail.com>
2011-03-16 02:08:53 +0500

    new file:   from_to_datetime_widtet.py

---
+++ from_to_datetime_widtet.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+import gtk
+from hiding_checkbutton import hiding_checkbutton
+from datetime_widget import datetime_widget
+
+class from_to_datetime_widtet(hiding_checkbutton):
+    def __init__(self, name):
+        
