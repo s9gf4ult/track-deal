2c6873c1ea2f1fc98b6d52d3e5c20498bd56132c
segfault <s9gf4ult@gmail.com>
2011-03-25 00:58:10 +0500

    select_control.py works fine

--- select_control.py
+++ select_control.py
@@ -1,5 +1,44 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+import gtk
 
 class select_control:
-    def __init__(self, 
+    def __init__(self, answers, checkbutton = None):
+        self.answers = answers
+        self.checkbutton = checkbutton
+
+    def get_value(self):
+        ret = None
+        for k in self.answers:
+            if self.answers[k].get_active():
+                ret = k
+                break
+        if self.checkbutton != None:
+            if self.checkbutton.get_active():
+                return ret
+        else:
+            return ret
+        return None
+            
+if __name__ == "__main__":
+    w = gtk.Dialog(buttons = (gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+    p = w.get_content_area()
+    c = gtk.CheckButton("Do that")
+    ans = {0 : gtk.RadioButton(label = "0")}
+    for x in xrange(1, 5):
+        yo = gtk.RadioButton(label = str(x), group = ans[0])
+        ans[x] = yo
+    scon = select_control(ans, c)
+    p.pack_start(c, False)
+    for x in ans:
+        p.pack_start(ans[x], False)
+    ent = gtk.Entry()
+    def clicked(bt):
+        ent.set_text("{0}".format(scon.get_value()))
+    b = gtk.Button("push")
+    b.connect("clicked", clicked)
+    p.pack_start(ent, False)
+    p.pack_start(b)
+    w.show_all()
+    w.run()
+
