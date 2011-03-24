5a592c922d992531d210f2bf714b9cd836a16f24
segfault <s9gf4ult@gmail.com>
2011-03-25 00:04:52 +0500

    number_range_control.py

--- number_range_control.py
+++ number_range_control.py
@@ -2,4 +2,22 @@
 # -*- coding: utf-8 -*-
 
 class number_range_control:
-    def __init__(self,  
+    def __init__(self, spin_button, checkbutton = None, step_incr = 1):
+        self.checkbutton = checkbutton
+        self.spin_button = spin_button
+        self.spin_button.get_adjustment().set_step_increment(step_incr)
+
+    def set_lower_limit(self, limit):
+        self.spin_button.get_adjustment().set_lower(limit)
+
+    def set_upper_limit(self, limit):
+        self.spin_button.get_adjustment().set_upper(limit)
+
+    def get_value(self):
+        val = self.spin_button.get_value()
+        if self.checkbutton != None:
+            self.checkbutton.get_active():
+                return val
+        else:
+            return val
+        return None
