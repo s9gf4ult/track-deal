fdb4354f579bb66ae8915499b640fbf2cc77cecf
segfault <s9gf4ult@gmail.com>
2011-03-25 00:17:37 +0500

    two number class

--- number_range_control.py
+++ number_range_control.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-class number_range_control:
+class number_control:
     def __init__(self, spin_button, checkbutton = None, step_incr = 1):
         self.checkbutton = checkbutton
         self.spin_button = spin_button
@@ -21,3 +21,28 @@ class number_range_control:
         else:
             return val
         return None
+
+class number_range_control:
+    def __init__(self, checkbutton, low_control, high_control):
+        self.checkbutton = checkbutton
+        self.low_control = low_control
+        self.high_control = high_control
+
+    def set_lower_limit(self, limit):
+        self.low_control.set_lower_limit(limit)
+        self.high_control.set_lower_limit(limit)
+
+    def set_upper_limit(self, limit):
+        self.low_control.set_upper_limit(limit)
+        self.high_control.set_upper_limit(limit)
+
+    def get_lower_value(self):
+        if self.checkbutton.get_active():
+            return self.low_control.get_value()
+        return None
+
+    def get_upper_value(self):
+        if self.checkbutton.get_active():
+            return self.high_control.get_value()
+        return None
+        
