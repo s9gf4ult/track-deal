eb82a05aa21eb27edc1eabd3de16faee671c781d
segfault <razor@calculate.local>
2011-03-25 12:25:28 +0500

    *** empty log message ***

--- list_view_control.py
+++ list_view_control.py
@@ -26,12 +26,37 @@ class list_view_control:
             elif isinstance(columns[k][1], gtk.CellRendererPixbuf):
                 prop["pixbuf"] = k
                 self.model_columns.append(gtk.gdk.Pixbuf)
-            c = gtk.TreeViewColumn(title = columns[k][0], cell_renderer = columns[k][1], **prop)
+            c = gtk.TreeViewColumn(columns[k][0], columns[k][1], **prop)
             c.set_clickable(True)
             c.connect("clicked", self.column_clicked, k)
             self.treeview.append_column(c)
             
     def make_model(self):
-        m = gtk.ListStore(*self.model_columns)
+        m = self.get_new_store()
+        self.treeview.set_model(m)
+
+    def get_new_store(self):
+        return gtk.ListStore(*self.model_columns)
         
-        
+    def column_clicked(self, column, col_id):
+        sid = self.treeview.get_model().get_sort_column_id()
+        if sid == col_id:
+            column.get
+        self.treeview.get_model().set_sort_column_id(col_id, gtk.SORT_ASCENDING)
+
+    def update_rows(self, rows):
+        m = self.get_new_store()
+        for row in rows:
+            m.append(row)
+        self.treeview.set_model(m)
+
+if __name__ == "__main__":
+    w = gtk.Dialog()
+    p = w.get_content_area()
+    v = gtk.TreeView()
+    p.pack_start(v)
+    con = list_view_control(v, [("OK", gtk.CellRendererToggle()), ("Name", gtk.CellRendererText())])
+    con.update_rows([(True, "ijij"), (True, "isejfj"), (False, "jeifjjj2")])
+    w.show_all()
+    w.run()
+    
