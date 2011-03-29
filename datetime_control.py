68d552b08ca2c5c9b73a6b86f0a7524b92433d95
segfault <s9gf4ult@gmail.com>
2011-03-29 23:24:54 +0600

    *** empty log message ***

--- datetime_control.py
+++ datetime_control.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 import gtk
 import datetime
+import time
+from hide_control import value_returner_control
 
-class datetime_control:
+class datetime_control(value_returner_control):
     def __init__(self, calendar, time_control, checkbutton = None):
         self.calendar = calendar
         self.time_control = time_control
@@ -12,22 +14,45 @@ class datetime_control:
     def get_date(self):
         date = self.calendar.get_date()
         val = datetime.date(date[0], date[1] + 1, date[2])
-        if self.checkbutton != None:
-            if self.checkbutton.get_active():
-                return val
-        else:
-            return val
-        return None
+        return self.return_value(val)
 
     def get_time(self):
-        if self.checkbutton.get_active() and self.time.checkbutton.get_active():
-            return datetime.time(self.time_widget.get_hour(), self.time_widget.get_min(), self.time_widget.get_sec())
-        else:
-            return None
+        return self.return_value(self.time_control.get_time())
+
+    def set_current_time(self):
+        self.time_control.set_current_time()
+
+    def set_current_date(self):
+        dd = datetime.datetime.fromtimestamp(time.time())
+        self.calendar.select_month((dd.month - 1), dd.year)
+        self.calendar.select_day(dd.day)
+
+    def set_current_datetime(self):
+        self.set_current_time()
+        self.set_current_date()
 
 if __name__ == "__main__":
-    w = gtk.Window()
-    w.connect("delete-event", gtk.main_quit)
-    w.add(datetime_widget(u'datetime').get_widget())
+    from time_control import *
+    w = gtk.Dialog()
+    p = w.get_content_area()
+    cdt = gtk.CheckButton("all")
+    cal = gtk.Calendar()
+    ct = gtk.CheckButton("time")
+    h = gtk.SpinButton()
+    m = gtk.SpinButton()
+    s = gtk.SpinButton()
+    tcon = time_control(h, m, s, ct)
+    dtcon = datetime_control(cal, tcon, cdt)
+    b = gtk.Button("push")
+    l = gtk.Label()
+    def clicked(bt):
+        l.set_text("{0} - {1}".format(dtcon.get_date(), dtcon.get_time()))
+    b.connect("clicked", clicked)
+    def clickedb(bt):
+        dtcon.set_current_datetime()
+    bb = gtk.Button("set curront")
+    bb.connect("clicked", clickedb)
+    for wi in [cdt, cal, ct, h, m, s, b, l, bb]:
+        p.pack_start(wi)
     w.show_all()
-    gtk.main()
+    w.run()
