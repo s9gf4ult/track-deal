f61be5db791da2792d833fe832381b5efcaafbcf
segfault <s9gf4ult@gmail.com>
2011-03-29 23:05:44 +0600

    *** empty log message ***

--- number_range_control.py
+++ number_range_control.py
@@ -1,8 +1,9 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import gtk
+from hide_control import value_returner_control
 
-class number_control:
+class number_control(value_returner_control):
     def __init__(self, spin_button, checkbutton = None, step_incr = 1):
         self.checkbutton = checkbutton
         self.spin_button = spin_button
@@ -15,16 +16,10 @@ class number_control:
         self.spin_button.get_adjustment().set_upper(limit)
 
     def get_value(self):
-        val = self.spin_button.get_value()
-        if self.checkbutton != None:
-            if  self.checkbutton.get_active():
-                return val
-        else:
-            return val
-        return None
+        return self.return_value(self.spin_button.get_value())
 
-class number_range_control:
-    def __init__(self, checkbutton, low_control, high_control):
+class number_range_control(value_returner_control):
+    def __init__(self, low_control, high_control, checkbutton = None):
         self.checkbutton = checkbutton
         self.low_control = low_control
         self.high_control = high_control
@@ -38,14 +33,10 @@ class number_range_control:
         self.high_control.set_upper_limit(limit)
 
     def get_lower_value(self):
-        if self.checkbutton.get_active():
-            return self.low_control.get_value()
-        return None
+        return self.return_value(self.low_control.get_value())
 
     def get_upper_value(self):
-        if self.checkbutton.get_active():
-            return self.high_control.get_value()
-        return None
+        return self.return_value(self.high_control.get_value())
         
 if __name__ == "__main__":
     w = gtk.Dialog(buttons = (gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
@@ -57,7 +48,7 @@ if __name__ == "__main__":
     bb = gtk.SpinButton()
     x = number_control(aa, a)
     y = number_control(bb, b)
-    n = number_range_control(c, x, y)
+    n = number_range_control(x, y, c)
     n.set_lower_limit(0)
     n.set_upper_limit(100)
     for wi in [c, a, aa, b, bb]:
