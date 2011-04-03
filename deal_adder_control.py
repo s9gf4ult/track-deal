83b0dffefd77f10fef967e925af6bf9d50f980d2
segfault <s9gf4ult@gmail.com>
2011-04-03 20:52:09 +0600

    *** empty log message ***

--- deal_adder_control.py
+++ deal_adder_control.py
@@ -1,7 +1,15 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+from datetime_control import *
+from combo_control import *
+from number_range_control import number_control
+from time_control import *
 
 class deal_adder_control:
     def __init__(self, builder):
         self.builder = builder
+        self.datetime = datetime_control(self.builder.get_object("deal_adder_calendar"),
+                                         time_control(self.builder.get_object("deal_adder_hour"),
+                                                      self.builder.get_object("deal_adder_min"),
+                                                      self.builder.get_object("deal_adder_sec")))
         
