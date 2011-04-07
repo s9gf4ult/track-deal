f2c0556279be076ab75e5f9be13d0e391de15f99
segfault <s9gf4ult@gmail.com>
2011-04-08 00:43:37 +0600

    *** empty log message ***

---
+++ positions_tab_controller.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+from modifying_tab_control import modifying_tab_control
+
+class positions_tab_controller(modifying_tab_control):
+    def __init__(self, database, builder, update_callback):
+        self.database = database
+        self.builder = builder
+        self.update_callback = update_callback
+        def shorter(name, action, *method):
+            self.builder.get_object(name).connect(action, *method)
+
+        shorter("positions_make", "activate", self.make_positions_activate)
+
+    def make_positions_activate(self, action):
+        self.make_positions()
+
+    def make_positions(self):
+        
+    
