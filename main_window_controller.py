8b676e8ee5d0a11fb9398c0f4ddac07df43d4f7c
segfault <razor@calculate.local>
2011-04-07 17:52:13 +0600

    *** empty log message ***

---
+++ main_window_controller.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+class main_windows_controller:
+    def __init__(self, database, builder):
+        self.database = database
+        self.builder = builder
+        shorter(name, signal, *method):
+            self.builder.get_object(name).connect(signal, *methid)
+
+        shorter("quit", "activate", self.quit_activate)
+        shorter("create_database", "activate", self.create_database_activate)
+        shorter("open_database", "activate", self.open_database_activate)
