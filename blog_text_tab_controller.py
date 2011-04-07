b6accf8051745462f2d1f320ce8c4f3b54479cc8
segfault <razor@calculate.local>
2011-04-07 12:46:33 +0600

    modified:   blog_text_tab_controller.py

--- blog_text_tab_controller.py
+++ blog_text_tab_controller.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+from list_view_sort_control import list_view_sort_control
 
 class blog_text_tab_controller:
     def __init__(self, database, builder):
@@ -10,4 +11,22 @@ class blog_text_tab_controller:
         self.builder = builder
         shorter("stock_view", "cursor-changed", self.stock_cursor_changed)
         shorter("date_view", "cursor-changed", self.date_cursor_changed)
+        self.blog_text = self.builder.get_object("blog_text_view")
+        self.stock_view = self.builder.get_object("stock_view")
+        self.date_view = self.builder.get_object("date_view")
+        self.date_view_control = list_view_sort_control(self.date_view, [("id", gtk.CellRendererSpin(), int, "id"), (u'Открытие', gtk.CellRendererText(), str, "open_datetime"), (u'Закрытие', gtk.CellRendererText(), str, "close_datetime"), (u'Количество', gtk.CellRendererSpin(), int, "count")], self_sorting = False, sort_callback = self.resort_date_view)
+        self.stock_view_control = list_view_sort_control(self.stock_view, [(u'Инструмент', gtk.CellRendererText(), str, "ticket")], self_sorting = False, sort_callback = self.resort_stock_view)
         
+
+    def update_widget(self):
+        pass
+
+    def stock_cursor_changed(self, treeview):
+        pass
+
+    def date_cursor_changed(self, treeview):
+        pass
+
+    def update_blog_text(self):
+        pass
+    
