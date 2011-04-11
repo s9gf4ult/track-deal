bd5533bede435889b3c9a597faf532a26876e174
segfault <s9gf4ult@gmail.com>
2011-04-12 00:20:20 +0600

    *** empty log message ***

--- account_edit_control.py
+++ account_edit_control.py
@@ -1,3 +1,12 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+import gtk
+
+class account_edit_control:
+    def __init__(self, builder):
+        self.builder = builder
+        def shobject(name):
+            return self.builder.get_object(name)
+        self.window = shobject("account_edit")
+        self.window.add_buttons(gtk.STOCK_OK, gtk.RESPONSE_ACCEPT, gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
