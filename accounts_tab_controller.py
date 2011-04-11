06d1ae8f421c79825819b82d764ce5972d13bc28
segfault <s9gf4ult@gmail.com>
2011-04-11 23:49:12 +0600

    modified:   accounts_tab_controller.py

--- accounts_tab_controller.py
+++ accounts_tab_controller.py
@@ -14,13 +14,19 @@ class accounts_tab_controller(modifying_tab_control):
         shorter("add_account", self.add_account_activate)
         shorter("delete_account", self.delete_account_activate)
         shorter("modify_account", self.modify_account_activate)
-        self.accounts_list = list_view_sort_control(self.builder.get_object("accounts_view"), [(u'Имя', gtk.CellRendererText()), (u'Начальный счет', gtk.CellRendererSpin()), (u'Текущий счет', gtk.CellRendererSpin())])
+        self.accounts_list = list_view_sort_control(self.builder.get_object("accounts_view"), [(u'Имя', gtk.CellRendererText()), (u'Начальный счет', gtk.CellRendererSpin()), (u'Текущий счет', gtk.CellRendererSpin()), (u'Валюта', gtk.CellRendererText())])
         self.account_list = list_view_sort_control(self.builder.get_object("account_view"), [(u'Свойство', gtk.CellRendererText()), (u'Значение', gtk.CellRendererText())])
         
 
     def update_widget(self):
-        pass
-    
+        self.update_accounts_list()
+
+    def update_accounts_list(self):
+        if self.database.connection:
+            u = self.database.connection.execute("select a.name, a.first_money, (a.first_money + sum(d.deal_sign * d.volume)), a.currency from accounts a inner join deals d on d.account_id = a.id where d.not_actual is null group by a.name").fetchall()
+            u += self.database.connection.execute("select a.name, a.first_money, a.first_money, a.currency from accounts a where not exists(select * from deals where account_id = a.id and not_actual is null)").fetchall()
+            self.accounts_list.update_rows(u)
+            
     def add_account_activate(self, action):
         self.add_account()
 
