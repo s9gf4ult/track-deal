bf849eedf086cf8dad88fa9f13d66009fc3fb3a8
segfault <razor@calculate.local>
2011-04-18 16:20:16 +0600

    *** empty log message ***

--- query_gen.py
+++ query_gen.py
@@ -1,3 +1,12 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+class query_gen():
+    in_work = False
+    returnings = []
+    conditions = []
+    
+    def __init__(self, tables, joins):
+        """tables is a list of tuples [(table_name, alias)], joins is a hash table like that {"=" : [((alias, field), (alias, field))], ">" : [((alias, field), (alias, field))]}"""
+        self.tables = tables
+        self.joins = joins
