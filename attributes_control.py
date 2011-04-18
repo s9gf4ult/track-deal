2d66f149d69288d67f55dd154be5d27f1a5841c1
razor <razor@comp-Celeron-CPU-42b97d.localdomain>
2011-04-19 00:40:04 +0600

    *** empty log message ***

--- attributes_control.py
+++ attributes_control.py
@@ -1,6 +1,9 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+from list_view_sort_control import list_view_sort_control
+import gtk
+
 class attributes_control:
     def __init__(self, treeview, name_entry, val_entry, add_button, del_button):
         self.treeview = treeview,
@@ -8,4 +11,24 @@ class attributes_control:
         self.val_entry = val_entry
         self.add_button = add_button
         self.del_button = del_button
-        
+        self.attributes = list_view_sort_control(self.treeview, [(u'Имя', gtk.CellRendererText()), (u'Значение', gtk.CellRendererText())])
+        self.add_button.connect("clicked", self.add_clicked)
+        self.del_button.connect("clicked", self.del_clicked)
+        self.treeview.connect("cursor-changed", self.cursor_changed)
+
+    def add_clicked(self, bt):
+        if len(self.name_entry.get_text()) > 0:
+            self.attributes.add_rows([(self.name_entry.get_text(), self.val_entry.get_text())])
+
+    def del_clicked(self, bt):
+        (mod, it) = self.treeview.get_selection().get_selected()
+        if it != None:
+            mod.remove(it)
+
+    def cursor_changed(self, tw):
+        (mod, it) = self.treeview.get_selection().get_selected()
+        if it != None:
+            self.name_entry.set_text(mod.get_value(it, 0))
+            self.val_entry.set_text(mod.get_value(it, 1))
+            
+            
