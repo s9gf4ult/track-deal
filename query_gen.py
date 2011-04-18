b46ba3c6a70797cadf1347a24aa5c881dce409ac
segfault <razor@calculate.local>
2011-04-18 18:07:12 +0600

    modified:   query_gen.py

--- query_gen.py
+++ query_gen.py
@@ -5,14 +5,14 @@ class query_gen():
     """class to generate queries to the database"""
     in_work = False
     returnings = []
-    conditions = []
+    conditions = ["and"]
     force_joins = []
     """list of aliases to force join tables"""
     tables = []
     joins = []
     
     def __init__(self, tables, joins):
-        """tables is a list of tuples [(table_name, alias)], joins is a hash table like that {"=" : [((alias, field), (alias, field))], ">" : [((alias, field), (alias, field))]}"""
+        """tables is a list of tuples [(table_name, alias)], joins is a hash table like that [("="  ((alias, field), (alias, field))), (">" : ((alias, field), (alias, field)))]"""
         self.tables = tables
         self.joins = joins
 
@@ -41,7 +41,21 @@ class query_gen():
         self.force_joins += aliases
 
     def add_conditions(self, conditions):
+        """conditions is a list like that ["and" ((alias, field), "=", (alias2, field2)), ((alias3, field3), ">", (alias4, field4)), ((alis5, field5), "between", 10, "and", 20)]"""
         self.check_working()
         self.conditions += conditions
         
+    def end(self):
+        rets = reduce(lambda a, b: u'{0}, {1}'.format(a, b),
+                      map(lambda x: u'{0}.{1}'.format(*x), self.returnings))
+        jtables = sorted(set(map(lambda a: a[0], self.returnings) + self.force_joins + self.get_tables_from_conditions()))
 
+    def get_tables_from_conditions(self):
+        return self._get_tables_from_conditions(self.conditions)
+
+    def _get_tables_from_conditions(self, conditions):
+        if not isinstance(conditions, list):
+            raise Exception("conditions must start from list")
+        if conditions[0] not in ["and", "or"]:
+            raise Exception("conditions must start from 'and' or 'or'")
+        
