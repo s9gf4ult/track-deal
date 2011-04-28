8e507ce015c216c8562360c6755ee0d0ae4aef84
segfault <s9gf4ult@gmail.com>
2011-04-28 21:39:58 +0600

    modified:   hiding_datetime_range_control.py 	modified:   main_ui.glade

--- hiding_datetime_range_control.py
+++ hiding_datetime_range_control.py
@@ -11,8 +11,8 @@ class hiding_datetime_range_control(datetime_range_control):
     def __init__(self, lower, upper, box = [], checkbutton = None):
         self.hiders = []
         for (bt, boxes) in [(checkbutton, box)] + reduce(lambda a, b: a + b, map(lambda h: [(gethash(h, "time_chbt"), [gethash(h, "time_box")]),
-                                                                                              (gethash(h, "chbt"), [gethash(h, "box")])],
-                                                                                   [lower, upper])):
+                                                                                            (gethash(h, "chbt"), [gethash(h, "box")])],
+                                                                                 [lower, upper])):
             rbox = filter(lambda a: a != None, boxes)
             if bt != None and len(rbox) > 0:
                 self.hiders.append(hide_control(bt, rbox))
@@ -21,10 +21,14 @@ class hiding_datetime_range_control(datetime_range_control):
             self.subhide = all_checked_control(checkbutton, subhide)
         times = map(lambda h: time_control( *map(lambda k: gethash(h, k), ["hour", "min", "sec", "time_chbt"])),
                     [lower, upper])
-        dtimes = map(lambda cal, time, chbt: datetime_control(cal, time, chbt),
+        dtimes = map(lambda cal, time, chbt, year, month, day: datetime_control(cal, time, chbt, year, month, day),
                      map(lambda a: gethash(a, "calendar"), [lower, upper]),
                      times,
-                     map(lambda a: gethash(a, "chbt"), [lower, upper]))
+                     map(lambda a: gethash(a, "chbt"), [lower, upper]),
+                     map(lambda a: gethash(a, "year"), [lower, upper]),
+                     map(lambda a: gethash(a, "month"), [lower, upper]),
+                     map(lambda a: gethash(a, "day"), [lower, upper]))
+                     
         datetime_range_control.__init__(self, *(dtimes + [checkbutton]))
         
 
