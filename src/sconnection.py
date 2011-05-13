06d9b8fc3e3217d7dc69ff320653df945a77b6cd
segfault <razor@calculate.local>
2011-05-13 16:10:52 +0600

    *** empty log message ***

---
+++ src/sconnection.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## sconnection ##
+
+import sqlite3
+
+class sconnection(sqlite3.Connection):
+    """wrapper over sqlite connection
+    implementing some usefull things
+    Attributes:
+    
+    """
+    ##############
+    # Attributes #
+    ##############
+    
+    
+    ###########
+    # Methods #
+    ###########
+
+    
