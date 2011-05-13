9ab4e68bffe36db5a506aeb7bf1e681492e265e1
segfault <razor@calculate.local>
2011-05-13 16:11:07 +0600

    *** empty log message ***

---
+++ src/sqlite_model.py
@@ -0,0 +1,39 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## sqlite_model ##
+
+from common_model import common_model
+from common_view import common_view
+from sconnection import sconnection
+
+class sqlite_model(common_model):
+    """stores data in sqlite
+    Attributes:
+    _connection_string = None
+    _sqlite_connection = None
+    
+    """
+    ##############
+    # Attributes #
+    ##############
+    _connection_string = None
+    _sqlite_connection = None
+    
+    
+    ###########
+    # Methods #
+    ###########
+    
+    def __init__(self, ):
+        """new instance of sqlite_model
+        """
+        pass
+
+    def connect(self, connection_string):
+        """connects to file or memory
+        
+        Arguments:
+        - `connection_string`:
+        """
+        assert(isinstance(connection_string, basestring))
+        self._sqlite_connection = sconnection(connection_string)
