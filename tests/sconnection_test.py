18f57692cc8e6d177c478b54d07f41266f421e82
segfault <s9gf4ult@gmail.com>
2011-05-15 18:19:52 +0600

    modified:   tests/sconnection_test.py

--- tests/sconnection_test.py
+++ tests/sconnection_test.py
@@ -25,6 +25,30 @@ class sconnection_test(unittest.TestCase):
         """
         """
         self.conn = sconnection.sconnection(":memory:")
+        self.conn.execute("create table aa(id, val)")
+
+    def test_insert(self, ):
+        """tests insert method
+        """
+        for c in xrange(0, 100):
+            self.conn.insert("aa", {"id" : c, "val" : 10})
+            
+        ins = []
+        for c in xrange(100, 200):
+            ins.append({"id" : c, "val" : 20})
+        self.conn.insert("aa", ins)
+        self.assertAlmostEqual(self.conn.execute("select count(*) from aa").fetchone()[0], 200)
+        self.assertAlmostEqual(self.conn.execute("select count(*) from aa where val = 20").fetchone()[0], 100)
+        self.assertAlmostEqual(self.conn.execute("select sum(val) from aa").fetchone()[0], 100 * 10 + 100 * 20)
+
+    def test_select(self, ):
+        """test of execute_select method
+        """
+        pass
+
+            
+            
+            
         
 
 
