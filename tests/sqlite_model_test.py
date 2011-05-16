ca2e17f5618084977a2856ef3e9fdeff658526d0
segfault <razor@calculate.local>
2011-05-16 17:50:29 +0600

    modified:   tests/sqlite_model_test.py

--- tests/sqlite_model_test.py
+++ tests/sqlite_model_test.py
@@ -1,2 +1,43 @@
 import sqlite_model
-import unit
+import unittest
+import sqlite3
+
+class sqlite_model_test(unittest.TestCase):
+    """
+    tests sqlite model class
+    Attributes:
+    
+    """
+    ##############
+    # Attributes #
+    ##############
+    
+    
+    ###########
+    # Methods #
+    ###########
+    
+    def setUp(self, ):
+        """
+        """
+        self.model = sqlite_model.sqlite_model()
+        self.model.connect(":memory:")
+
+    def test_dbinit(self, ):
+        """tests dbinit execution
+        """
+        self.model.dbinit()
+
+    def test_dbtemp(self, ):
+        """tests dbtemp execution
+        """
+        self.assertRaises(sqlite3.OperationalError, self.model.dbtemp)
+        self.model = sqlite_model.sqlite_model()
+        self.model.connect(":memory:")
+        self.model.dbinit()
+        self.model.dbtemp()
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
