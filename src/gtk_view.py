8d8ec50453571433118f26ba098b4db880b80b8e
segfault <razor@calculate.local>
2011-06-06 12:16:11 +0600

    new file:   src/gtk_view.py 	new file:   src/start_gtk.py

---
+++ src/gtk_view.py
@@ -0,0 +1,7 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## gtk_view ##
+
+class gtk_view(common_view):
+    """Open deals Gtk view class (gtk interface for open-deals)
+    """
---
+++ src/start_gtk.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## start_gtk ##
+
+
+if __name__ == '__main__':
+    import sqlite_model
+    import gtk_view
+    import sys
+    import application
+    a = application.application(gtk_view.gtk_view())
+    if len(sys.argv) > 1:
+        m = sqlite_model.sqlite_model()
+        m.open_existing(sys.argv[1])
+        a.start(m)
+    else:
+        a.start()
