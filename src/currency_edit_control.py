37ec859f00f8a803b5d0dfa0eade5c78f86ec534
segfault <s9gf4ult@gmail.com>
2011-06-28 01:46:28 +0600

    currency_edit_control.py

---
+++ src/currency_edit_control.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## currency_edit_control ##
+
+class currency_edit_control(object):
+    """\~russian
+    \brief Контрол для редактирования списка известных валют
+    """
+    def __init__(self, parent):
+        """
+        \param instance of \ref gtk_view.gtk_view
+        """
+        self._parent = parent
+        
