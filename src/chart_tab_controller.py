5c7aaef24ea53530c2e40ed7da979b548982e51d
segfault <razor@localhost.(none)>
2011-07-30 00:50:33 +0600

    new file:   src/chart_tab_controller.py

---
+++ src/chart_tab_controller.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## chart_tab_controller ##
+
+class chart_tab_controller(object):
+    """\brief controller of tab "chart" of main window
+    """
+    def __init__(self, parent):
+        """\brief constructor
+        \param parent \ref gtk_view.gtk_view instance
+        """
+        self._parent = parent
+        
