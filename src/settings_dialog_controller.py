4a8e01c5b19057be510a7a333dc7930b2fe8c079
segfault <razor@calculate.local>
2011-08-05 18:00:54 +0600

    modified:   settings_dialog.glade 	new file:   ../src/settings_dialog_controller.py

---
+++ src/settings_dialog_controller.py
@@ -0,0 +1,47 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+## settings_dialog_controller ##
+from common_methods import make_builder
+
+class settings_dialog_controller(object):
+    """\brief controller for settings dialog
+    """
+    def __init__(self, parent):
+        """\brief
+        \param parent - \ref gtk_view.gtk_view instance
+        """
+        self._parent = parent
+        self.builder = make_builder('glade/settings_dialog.glade')
+        def shobject(name):
+            return self.builder.get_object(name)
+
+        self.window = shobject('settings_dialog')
+        self.window.add_buttons(gtk.STOCK_SAVE, gtk.RESPONSE_ACCEPT, gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+        self.window.set_transient_for(self._parent.window.builder.get_object('main_window'))
+        self.load_settings()
+
+    def run(self, ):
+        """\brief run dialog and save settings if SAVE pressed
+        \retval gtk.RESPONSE_ACCEPT
+        \retval gtk.RESPONSE_CANCEL
+        """
+        ret = self.window.run()
+        if ret == gtk.RESPONSE_ACCEPT:
+            self.save_settings()
+        return ret
+
+    def save_settings(self, ):
+        """\brief save setting into settings instance
+        """
+        raise NotImplementedError()
+
+    def load_settings(self, ):
+        """\brief load settings from settings instance into dialog
+        """
+        raise NotImplementedError()
+
+
+
+                                      
+
+        
