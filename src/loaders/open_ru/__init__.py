0a7763c0bc76a6a7d6e7f285b3330cb44e750bbb
segfault <razor@localhost.(none)>
2011-10-17 02:12:23 +0600

    modified:   src/loaders/open_ru/__init__.py

--- src/loaders/open_ru/__init__.py
+++ src/loaders/open_ru/__init__.py
@@ -7,3 +7,14 @@ def get_dialog_class():
     when importing the data from some stuff
     """
     return None
+
+def get_icon_filename():
+    """\brief return string with full path to icon
+    """
+    return None
+
+def get_name():
+    """\brief return string with name of loader
+    """
+    return u'Отчет брокера "Открытие"'
+
